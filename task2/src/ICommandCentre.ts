9ba75a112b4aff74cf4002dc2e63cb5d7874dd9a
Ігнатюк Андрій Мирославович <ipz231_iam@student.ztu.edu.ua>
2026-03-25 23:58:58 +0200

    feat: add task5

--- task2/src/ICommandCentre.ts
+++
@@ -1,9 +0,0 @@
-import { Aircraft } from './Aircraft';
-import { Runway } from './Runway';
-
-export interface ICommandCentre {
-    requestLanding(aircraft: Aircraft): void;
-    requestTakeOff(aircraft: Aircraft): void;
-    registerRunway(runway: Runway): void;
-    registerAircraft(aircraft: Aircraft): void;
-}
